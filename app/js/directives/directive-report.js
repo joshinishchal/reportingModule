55416e9b21814b58b04f435dc169d0e4d906d6b7
netpulse <njoshi@netpulse.com>
2016-09-20 13:15:34 -0700

    making sure that we can remove previous state and add a new state when status changes

--- app/js/directives/directive-report.js
+++ app/js/directives/directive-report.js
@@ -35,11 +35,13 @@ reportModule.directive("report",[function(){
         "link" : function(scope, element, attr){
             scope.$watch("state", function(){
                 if(scope.state.toLowerCase() == "error"){
+                    angular.element(angular.element.find("#stateDefination i")).remove();
                     angular.element(angular.element.find("#stateDefination")).prepend("<i class='fa fa-exclamation-circle'></i>").append("<i class='fa fa-ellipsis-h'></i>");
                 }else if(scope.state.toLowerCase() == "ok"){
+                    angular.element(angular.element.find("#stateDefination i")).remove();
                     angular.element(angular.element.find("#stateDefination")).prepend("<i class='fa fa-check-circle'></i>").append("<i class='fa fa-ellipsis-h'></i>");
                 }
-            });
+            }, true);
         }
     };
 }]);
